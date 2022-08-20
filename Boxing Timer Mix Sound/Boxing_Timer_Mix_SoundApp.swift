bcaf7f407c6ef728ad31bb20351fd85dc76f03a1
Len Lin <rlinlen@gmail.com>
2022-08-20 21:10:24 +0800

    Initial Commit

---
+++ Boxing Timer Mix Sound/Boxing_Timer_Mix_SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boxing_Timer_Mix_SoundApp.swift
+//  Boxing Timer Mix Sound
+//
+//  Created by Len Lin on 2022/8/20.
+//
+
+import SwiftUI
+
+@main
+struct Boxing_Timer_Mix_SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boxing Timer Mix Sound/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Boxing Timer Mix Sound
+//
+//  Created by Len Lin on 2022/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
